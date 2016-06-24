0fc2664aadaebf7f9f9f06ce565dc18bd067e3ea
Nicholas-Liu <liujun1861@126.com>
2016-06-24 17:51:21 +0800

    add class active again

--- daterangepicker.js
+++ daterangepicker.js
@@ -811,7 +811,7 @@
 
                     //highlight dates in-between the selected dates
                     if (this.endDate != null && calendar[row][col] > this.startDate && calendar[row][col] < this.endDate)
-                        classes.push('in-range');
+                        classes.push('active', 'in-range');
 
                     //apply custom classes for this date
                     var isCustom = this.isCustomDate(calendar[row][col]);
